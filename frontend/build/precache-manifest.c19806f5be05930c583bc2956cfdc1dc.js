db4d57512180fd9402fc4aa0f4e7effa5f217189
Gary Shen <gshen7@uwo.ca>
2020-06-16 00:50:35 -0400

    add build

---
+++ frontend/build/precache-manifest.c19806f5be05930c583bc2956cfdc1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a287096e6371e626d4d26936fcd52915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5e43f64922aa47afad1",
+    "url": "/static/css/main.31c46d37.chunk.css"
+  },
+  {
+    "revision": "b432ecf19deaae94fec4",
+    "url": "/static/js/2.e59d9b4b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e59d9b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e43f64922aa47afad1",
+    "url": "/static/js/main.0e025776.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
