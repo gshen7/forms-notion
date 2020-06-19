60f8333c726d54dcf252de6f14376e7b5523075e
Gary Shen <gshen7@uwo.ca>
2020-06-19 16:20:57 -0400

    build form loading spinner

---
+++ frontend/build/precache-manifest.30396ffaf7c2dc5c7ef98e243d536eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2c6a6682e1c6100477dc6c5d8cf029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61fa8aef24de78033a0",
+    "url": "/static/css/main.31c46d37.chunk.css"
+  },
+  {
+    "revision": "6cc736e674bfa19fb69d",
+    "url": "/static/js/2.fdb1c1b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fdb1c1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61fa8aef24de78033a0",
+    "url": "/static/js/main.338f01a8.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
